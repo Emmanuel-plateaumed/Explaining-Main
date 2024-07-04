ec752f1178e46fbadfc38b9127b61a15c5bcfdf5
Chiemeke <echiemeke@plateaumed.com>
2024-07-04 23:37:18 +0100

    updated the document to include pull changes

--- diagram.dart
+++ diagram.dart
@@ -5,6 +5,8 @@ EMMA[Local Describing Emmanuel] ---> GITHUB[Remote Describing Emmanuel] ----> AB
 1. Ctrl s[Save my changes].
 2. Add all my changes[git add .]
 3. Add title to those changes I made so that I can reference later in future.[git commit -m "YOUR COMMIT MESSAGE"]
+4. Pull changes from remote branch
 4. Push my changes to the remote repository[git push] i.e GITHUB
 
 I want to show them what it means to track
+
