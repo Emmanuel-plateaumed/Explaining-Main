9e79d54e5bee906a722e99680046091db6d0b5d8
Chiemeke <echiemeke@plateaumed.com>
2024-07-04 23:32:05 +0100

    Demonstrating branch tracking

--- diagram.dart
+++ diagram.dart
@@ -1,3 +1,10 @@
 
 
-[Master] ---> [Describing Emmanuel]
+EMMA[Local Describing Emmanuel] ---> GITHUB[Remote Describing Emmanuel] ----> ABU[Local Describing Emmanuel]
+                                    
+1. Ctrl s[Save my changes].
+2. Add all my changes[git add .]
+3. Add title to those changes I made so that I can reference later in future.[git commit -m "YOUR COMMIT MESSAGE"]
+4. Push my changes to the remote repository[git push] i.e GITHUB
+
+I want to show them what it means to track
